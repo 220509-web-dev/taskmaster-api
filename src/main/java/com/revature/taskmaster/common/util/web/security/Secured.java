db70ea4860e93e206d475e7ec26be4bf4a035dc2
Wezley Singleton <wezley.singleton@gmail.com>
2022-06-24 22:04:47 -0400

    setup aop challenge

--- src/main/java/com/revature/taskmaster/common/util/web/security/Secured.java
+++ src/main/java/com/revature/taskmaster/common/util/web/security/Secured.java
@@ -1,5 +1,10 @@
 package com.revature.taskmaster.common.util.web.security;
 
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Secured {
     String[] allowedRoles();
 }
