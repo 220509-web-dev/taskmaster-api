94df4f826d72d21a81a5df59d19f2c4b680aebb6
Wezley Singleton <wezley.singleton@gmail.com>
2022-06-29 10:58:17 -0400

    removed unused imports from UserRepo

--- src/main/java/com/revature/taskmaster/user/UserRepository.java
+++ src/main/java/com/revature/taskmaster/user/UserRepository.java
@@ -1,11 +1,8 @@
 package com.revature.taskmaster.user;
 
-import com.revature.taskmaster.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
