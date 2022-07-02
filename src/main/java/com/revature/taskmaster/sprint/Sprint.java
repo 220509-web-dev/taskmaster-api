bf197ef2e6f3df72302efc9b4bf6e7a049fa84b3
Wezley Singleton <wezley.singleton@gmail.com>
2022-07-02 00:00:34 -0400

    base sprint entity implemented

--- src/main/java/com/revature/taskmaster/sprint/Sprint.java
+++ src/main/java/com/revature/taskmaster/sprint/Sprint.java
@@ -1,22 +1,115 @@
 package com.revature.taskmaster.sprint;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.revature.taskmaster.common.datasource.Resource;
+import com.revature.taskmaster.project.Project;
+import com.revature.taskmaster.task.Task;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Entity
-@Table(name = "sprints")
-public class Sprint {
+@Table(
+    name = "sprints",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"name", "project_id"})
+)
+public class Sprint extends Resource {
 
-    @Id
-    private String id;
+    @Column(nullable = false)
+    private String name;
 
-    public String getId() {
-        return id;
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDate endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToMany
+    @JoinTable(name = "sprint_tasks")
+    private List<Task> tasks;
+
+    public Sprint() {
+        super();
+        tasks = new ArrayList<>();
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTasks(Task... tasks) {
+        if (this.tasks == null) {
+            this.tasks = new ArrayList<>();
+        }
+        this.tasks.addAll(Arrays.asList(tasks));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sprint sprint = (Sprint) o;
+        return Objects.equals(name, sprint.name) && Objects.equals(project, sprint.project) && Objects.equals(tasks, sprint.tasks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, project, tasks);
+    }
+
+    @Override
+    public String toString() {
+        return "Sprint{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", projectId=" + project.getId() +
+                ", taskIds=" + tasks.stream().map(Task::getId).collect(Collectors.toList()) +
+                ", metadata=" + metadata +
+                '}';
     }
 
 }
