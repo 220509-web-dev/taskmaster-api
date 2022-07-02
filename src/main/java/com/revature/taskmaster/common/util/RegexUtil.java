16023796b54e4e3b6ff3ef5d0e7289aca94d0f2a
Wezley Singleton <wezley.singleton@gmail.com>
2022-07-01 23:58:04 -0400

    added private constructor for utility class (sonarlint rule enforced)

--- src/main/java/com/revature/taskmaster/common/util/RegexUtil.java
+++ src/main/java/com/revature/taskmaster/common/util/RegexUtil.java
@@ -2,4 +2,9 @@ package com.revature.taskmaster.common.util;
 
 public class RegexUtil {
     public static final String PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+
+    private RegexUtil() {
+        super();
+    }
+
 }
