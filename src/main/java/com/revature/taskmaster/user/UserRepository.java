8d8c9d05abe7316f3d0cf1a439d3903b230d1b71
Wezley Singleton <wezley.singleton@gmail.com>
2022-07-04 21:28:18 -0400

    removed unused query method

--- src/main/java/com/revature/taskmaster/user/UserRepository.java
+++ src/main/java/com/revature/taskmaster/user/UserRepository.java
@@ -3,12 +3,8 @@ package com.revature.taskmaster.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-
     boolean existsByUsername(String username);
     boolean existsByEmailAddress(String emailAddress);
-    Optional<User> findUserByEmailAddress(String emailAddress);
 }
