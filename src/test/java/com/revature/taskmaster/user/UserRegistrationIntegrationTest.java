31a75d41767c52dbc3a05937046f6da88a4f0072
Wezley Singleton <wezley.singleton@gmail.com>
2022-07-04 21:56:41 -0400

    added DirtiesContext annotation to reset app context after user registration integration test suite runs

--- src/test/java/com/revature/taskmaster/user/UserRegistrationIntegrationTest.java
+++ src/test/java/com/revature/taskmaster/user/UserRegistrationIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasItem;
@@ -16,15 +17,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@DirtiesContext
 @AutoConfigureMockMvc
 class UserRegistrationIntegrationTest {
 
     private final MockMvc mockMvc;
     private final ObjectMapper jsonMapper;
     private final UserRepository userRepo;
-
     private final String PATH = "/users";
-
     private final String CONTENT_TYPE = "application/json";
 
     @Autowired
