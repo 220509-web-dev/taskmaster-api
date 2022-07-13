66010c4a6e54be653e7ce6e0847753fdbb2ab70e
Wezley Singleton <wezley.singleton@gmail.com>
2022-07-12 21:24:57 -0400

    add @Component

--- src/main/java/com/revature/taskmaster/common/util/web/validators/KnownPriorityLevelValidator.java
+++ src/main/java/com/revature/taskmaster/common/util/web/validators/KnownPriorityLevelValidator.java
@@ -2,10 +2,12 @@ package com.revature.taskmaster.common.util.web.validators;
 
 import com.revature.taskmaster.common.util.web.validators.annotations.KnownPriorityLevel;
 import com.revature.taskmaster.task.Task;
+import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+@Component
 public class KnownPriorityLevelValidator implements ConstraintValidator<KnownPriorityLevel, Integer> {
     @Override
     public boolean isValid(Integer value, ConstraintValidatorContext context) {
