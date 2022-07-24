34b87da6ca0ced7f40e22e9e424994bcf0c6794f
Wezley Singleton <wezley.singleton@gmail.com>
2022-07-24 18:15:30 -0400

    add default constructor to AuthorizationException

--- src/main/java/com/revature/taskmaster/common/util/exceptions/AuthorizationException.java
+++ src/main/java/com/revature/taskmaster/common/util/exceptions/AuthorizationException.java
@@ -2,6 +2,10 @@ package com.revature.taskmaster.common.util.exceptions;
 
 public class AuthorizationException extends RuntimeException {
 
+    public AuthorizationException() {
+        super("You do not have the proper permissions to perform that operation.");
+    }
+
     public AuthorizationException(String message) {
         super(message);
     }
