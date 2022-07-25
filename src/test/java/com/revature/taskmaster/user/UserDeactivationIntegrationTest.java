78c9f289290378f2c2338e20d01ce7b927c8edc7
Wezley Singleton <wezley.singleton@gmail.com>
2022-07-25 00:21:15 -0400

    update user deactivation integration tests following security refactor; uses MockTokenGenerator

--- src/test/java/com/revature/taskmaster/user/UserDeactivationIntegrationTest.java
+++ src/test/java/com/revature/taskmaster/user/UserDeactivationIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.revature.taskmaster.user;
 
+import com.revature.taskmaster.test_utils.MockTokenGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,10 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Map;
+
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -22,38 +24,93 @@ class UserDeactivationIntegrationTest {
 
     private final MockMvc mockMvc;
     private final UserRepository userRepo;
+    private final MockTokenGenerator mockTokenGenerator;
     private final String PATH = "/users";
     private final String CONTENT_TYPE = "application/json";
+    private static final String MOCK_USER_NOT_LOADED = "Test active user not found, check mock data inserter to ensure it inserts an active user";
 
     @Autowired
-    public UserDeactivationIntegrationTest(MockMvc mockMvc, UserRepository userRepo) {
+    public UserDeactivationIntegrationTest(MockMvc mockMvc, UserRepository userRepo, MockTokenGenerator mockTokenGenerator) {
         this.mockMvc = mockMvc;
         this.userRepo = userRepo;
+        this.mockTokenGenerator = mockTokenGenerator;
     }
 
     @Test
-    void test_userActivation_returns204_givenKnownUserId() throws Exception {
+    void test_userDeactivation_returns204_givenKnownUserId_usingAdminToken() throws Exception {
 
-        User lockedUser = userRepo.findById("locked-user-id").orElse(null);
-        assertNotNull(lockedUser, "Test inactive user not found, check mock data inserter to ensure it inserts an inactive user");
+        User activeManagerUser = userRepo.findById("manager-user-id").orElse(null);
+        assertNotNull(activeManagerUser, MOCK_USER_NOT_LOADED);
 
-        mockMvc.perform(delete(PATH + "?id=" + lockedUser.getId()))
+        mockMvc.perform(
+                    delete(PATH)
+                        .header("Authorization", mockTokenGenerator.getAdminToken())
+                        .param("id", activeManagerUser.getId()))
                .andExpect(status().isNoContent())
                .andExpect(header().string("Access-Control-Allow-Origin", "*"))
                .andExpect(header().string("Access-Control-Allow-Methods", "*"))
                .andExpect(header().string("Access-Control-Allow-Headers", "*"))
                .andReturn();
 
-        User nowInactiveUser = userRepo.findById("locked-user-id").orElse(null);
+        User nowInactiveUser = userRepo.findById(activeManagerUser.getId()).orElse(null);
         assertNotNull(nowInactiveUser);
         assertFalse(nowInactiveUser.getMetadata().isActive());
 
     }
 
     @Test
-    void test_userActivation_returns404_givenUnknownUserId() throws Exception {
-        String unknownId = "unknown-id";
-        mockMvc.perform(delete(PATH + "?id=" + unknownId))
+    void test_userDeactivation_returns204_givenKnownUserId_usingResourceOwnerToken() throws Exception {
+
+        User activeTesterUser = userRepo.findById("tester-user-id").orElse(null);
+        assertNotNull(activeTesterUser, MOCK_USER_NOT_LOADED);
+
+        mockMvc.perform(
+                   delete(PATH)
+                       .header("Authorization", mockTokenGenerator.getTesterToken())
+                       .param("id", activeTesterUser.getId()))
+               .andExpect(status().isNoContent())
+               .andExpect(header().string("Access-Control-Allow-Origin", "*"))
+               .andExpect(header().string("Access-Control-Allow-Methods", "*"))
+               .andExpect(header().string("Access-Control-Allow-Headers", "*"))
+               .andReturn();
+
+        User nowInactiveUser = userRepo.findById(activeTesterUser.getId()).orElse(null);
+        assertNotNull(nowInactiveUser);
+        assertFalse(nowInactiveUser.getMetadata().isActive());
+
+    }
+
+    @Test
+    void test_userDeactivation_returns403_givenKnownUserId_usingNonResourceOwnerToken() throws Exception {
+
+        User activeManagerUser = userRepo.findById("manager-user-id").orElse(null);
+        assertNotNull(activeManagerUser, MOCK_USER_NOT_LOADED);
+
+        mockMvc.perform(
+                   delete(PATH)
+                       .header("Authorization", mockTokenGenerator.getDevToken())
+                       .param("id", activeManagerUser.getId()))
+               .andExpect(status().isForbidden())
+               .andExpect(header().string("Access-Control-Allow-Origin", "*"))
+               .andExpect(header().string("Access-Control-Allow-Methods", "*"))
+               .andExpect(header().string("Access-Control-Allow-Headers", "*"))
+               .andExpect(jsonPath("$.messages").isArray())
+               .andExpect(jsonPath("$.messages", hasSize(1)))
+               .andExpect(jsonPath("$.messages", hasItem("You do not have the proper permissions to perform that operation.")))
+               .andReturn();
+
+        User stillActiveUser = userRepo.findById(activeManagerUser.getId()).orElse(null);
+        assertNotNull(stillActiveUser);
+        assertTrue(stillActiveUser.getMetadata().isActive());
+
+    }
+
+    @Test
+    void test_userDeactivation_returns404_givenUnknownUserId_usingAdminToken() throws Exception {
+        mockMvc.perform(
+                   delete(PATH)
+                       .header("Authorization", mockTokenGenerator.getAdminToken())
+                       .param("id", "unknown-id"))
                .andExpect(status().isNotFound())
                .andExpect(header().string("content-type", CONTENT_TYPE))
                .andExpect(header().string("Access-Control-Allow-Origin", "*"))
@@ -63,6 +120,26 @@ class UserDeactivationIntegrationTest {
                .andExpect(jsonPath("$.messages", hasSize(1)))
                .andExpect(jsonPath("$.messages", hasItem("No resource found using the provided search params!")))
                .andReturn();
+    }
+
+    @Test
+    void test_userDeactivation_returns403_givenUnknownUserId_usingAnyNonAdminToken() throws Exception {
+        for (Map.Entry<User.Role, String> roleTokenEntries : mockTokenGenerator.getRoleTokens().entrySet()) {
+            if (roleTokenEntries.getKey().equals(User.Role.ADMIN)) continue;
+            mockMvc.perform(
+                       delete(PATH)
+                           .header("Authorization", roleTokenEntries.getValue())
+                           .param("id", "unknown-id"))
+                   .andExpect(status().isForbidden())
+                   .andExpect(header().string("content-type", CONTENT_TYPE))
+                   .andExpect(header().string("Access-Control-Allow-Origin", "*"))
+                   .andExpect(header().string("Access-Control-Allow-Methods", "*"))
+                   .andExpect(header().string("Access-Control-Allow-Headers", "*"))
+                   .andExpect(jsonPath("$.messages").isArray())
+                   .andExpect(jsonPath("$.messages", hasSize(1)))
+                   .andExpect(jsonPath("$.messages", hasItem("You do not have the proper permissions to perform that operation.")))
+                   .andReturn();
+        }
     }
 
 }
