89d4960f191f7ca6d8a8438271e15a55a5327dfe
Wezley Singleton <wezley.singleton@gmail.com>
2022-07-25 01:04:43 -0400

    now expecting new task creation with unknown user token to return 401 rather than 422; also suite was refactored to use mock token generator

--- src/test/java/com/revature/taskmaster/task/TaskCreationIntegrationTest.java
+++ src/test/java/com/revature/taskmaster/task/TaskCreationIntegrationTest.java
@@ -5,6 +5,7 @@ import com.revature.taskmaster.auth.TokenService;
 import com.revature.taskmaster.auth.dtos.Principal;
 import com.revature.taskmaster.common.dtos.ResourceCreationResponse;
 import com.revature.taskmaster.task.dtos.TaskRequestPayload;
+import com.revature.taskmaster.test_utils.MockTokenGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -29,19 +30,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class TaskCreationIntegrationTest {
 
     private final MockMvc mockMvc;
-    private final ObjectMapper jsonMapper;
-
-    private final TokenService tokenService;
+    private final MockTokenGenerator mockTokenGenerator;
     private final TaskRepository taskRepo;
+    private final ObjectMapper jsonMapper;
     private final String PATH = "/tasks";
     private final String CONTENT_TYPE = "application/json";
 
     @Autowired
-    public TaskCreationIntegrationTest(MockMvc mockMvc, ObjectMapper jsonMapper, TokenService tokenService, TaskRepository taskRepo) {
+    public TaskCreationIntegrationTest(MockMvc mockMvc, MockTokenGenerator mockTokenGenerator, TaskRepository taskRepo, ObjectMapper jsonMapper) {
         this.mockMvc = mockMvc;
-        this.jsonMapper = jsonMapper;
-        this.tokenService = tokenService;
+        this.mockTokenGenerator = mockTokenGenerator;
         this.taskRepo = taskRepo;
+        this.jsonMapper = jsonMapper;
     }
 
     @Test
@@ -55,14 +55,11 @@ class TaskCreationIntegrationTest {
         newTaskRequest.setDueDate(LocalDate.now().plusDays(14));
         newTaskRequest.setLabels(Arrays.asList("ready to start", "valid", "test"));
 
-        String token = tokenService.generateToken(new Principal("dev-user-id", "dev", "DEV"));
-        String requestPayload = jsonMapper.writeValueAsString(newTaskRequest);
-
         MvcResult result = mockMvc.perform(
                                     post(PATH)
-                                        .header("Authorization", token)
+                                        .header("Authorization", mockTokenGenerator.getDevToken())
                                         .contentType(CONTENT_TYPE)
-                                        .content(requestPayload))
+                                        .content(jsonMapper.writeValueAsString(newTaskRequest)))
                                   .andExpect(status().isCreated())
                                   .andExpect(header().string("content-type", CONTENT_TYPE))
                                   .andExpect(header().string("Access-Control-Allow-Origin", "*"))
@@ -90,14 +87,11 @@ class TaskCreationIntegrationTest {
         newTaskRequest.setLabels(Arrays.asList("ready to start", "valid", "test"));
         newTaskRequest.setAssigneeIds(Arrays.asList("dev-user-id", "tester-user-id"));
 
-        String token = tokenService.generateToken(new Principal("dev-user-id", "dev", "DEV"));
-        String requestPayload = jsonMapper.writeValueAsString(newTaskRequest);
-
         MvcResult result = mockMvc.perform(
                                     post(PATH)
-                                        .header("Authorization", token)
+                                        .header("Authorization", mockTokenGenerator.getDevToken())
                                         .contentType(CONTENT_TYPE)
-                                        .content(requestPayload))
+                                        .content(jsonMapper.writeValueAsString(newTaskRequest)))
                                   .andExpect(status().isCreated())
                                   .andExpect(header().string("content-type", CONTENT_TYPE))
                                   .andExpect(header().string("Access-Control-Allow-Origin", "*"))
@@ -114,7 +108,7 @@ class TaskCreationIntegrationTest {
     }
 
     @Test
-    void test_createNewTask_returns422_givenValidTaskRequestPayload_butUnknownCreator() throws Exception {
+    void test_createNewTask_returns401_givenValidTaskRequestPayload_butUnknownCreator() throws Exception {
 
         TaskRequestPayload newTaskRequest = new TaskRequestPayload();
         newTaskRequest.setTitle("Valid Task Title");
@@ -125,15 +119,12 @@ class TaskCreationIntegrationTest {
         newTaskRequest.setLabels(Arrays.asList("ready to start", "valid", "test"));
         newTaskRequest.setAssigneeIds(Arrays.asList("dev-user-id", "tester-user-id"));
 
-        String token = tokenService.generateToken(new Principal("fake-admin-user-id", "fake-admin", "ADMIN"));
-        String requestPayload = jsonMapper.writeValueAsString(newTaskRequest);
-
         mockMvc.perform(
                     post(PATH)
-                        .header("Authorization", token)
+                        .header("Authorization", mockTokenGenerator.getUnknownUserToken())
                         .contentType(CONTENT_TYPE)
-                        .content(requestPayload))
-               .andExpect(status().isUnprocessableEntity())
+                        .content(jsonMapper.writeValueAsString(newTaskRequest)))
+               .andExpect(status().isUnauthorized())
                .andExpect(header().string("content-type", CONTENT_TYPE))
                .andExpect(header().string("Access-Control-Allow-Origin", "*"))
                .andExpect(header().string("Access-Control-Allow-Methods", "*"))
@@ -155,14 +146,11 @@ class TaskCreationIntegrationTest {
         newTaskRequest.setLabels(Arrays.asList("ready to start", "valid", "test"));
         newTaskRequest.setAssigneeIds(Arrays.asList("dev-user-id", "unknown-user-id"));
 
-        String token = tokenService.generateToken(new Principal("dev-user-id", "dev", "DEV"));
-        String requestPayload = jsonMapper.writeValueAsString(newTaskRequest);
-
         mockMvc.perform(
                    post(PATH)
-                       .header("Authorization", token)
+                       .header("Authorization", mockTokenGenerator.getDevToken())
                        .contentType(CONTENT_TYPE)
-                       .content(requestPayload))
+                       .content(jsonMapper.writeValueAsString(newTaskRequest)))
                .andExpect(status().isUnprocessableEntity())
                .andExpect(header().string("content-type", CONTENT_TYPE))
                .andExpect(header().string("Access-Control-Allow-Origin", "*"))
@@ -184,14 +172,12 @@ class TaskCreationIntegrationTest {
         newTaskRequest.setDueDate(LocalDate.now().minusDays(14));
         newTaskRequest.setState("DONE");
 
-        String token = tokenService.generateToken(new Principal("dev-user-id", "dev", "DEV"));
-        String requestPayload = jsonMapper.writeValueAsString(newTaskRequest);
 
         mockMvc.perform(
                    post(PATH)
-                       .header("Authorization", token)
+                       .header("Authorization", mockTokenGenerator.getDevToken())
                        .contentType(CONTENT_TYPE)
-                       .content(requestPayload))
+                       .content(jsonMapper.writeValueAsString(newTaskRequest)))
                .andExpect(status().isBadRequest())
                .andExpect(header().string("content-type", CONTENT_TYPE))
                .andExpect(header().string("Access-Control-Allow-Origin", "*"))
@@ -215,7 +201,9 @@ class TaskCreationIntegrationTest {
 
         String requestPayload = "{invalid}";
 
-        mockMvc.perform(post(PATH).contentType(CONTENT_TYPE).content(requestPayload))
+        mockMvc.perform(post(PATH)
+                           .contentType(CONTENT_TYPE)
+                           .content(requestPayload))
                .andExpect(status().isBadRequest())
                .andExpect(header().string("content-type", CONTENT_TYPE))
                .andExpect(header().string("Access-Control-Allow-Origin", "*"))
